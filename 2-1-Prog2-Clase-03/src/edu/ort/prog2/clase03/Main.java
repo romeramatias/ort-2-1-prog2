36db63bbead5787cf59b487a2ee24a761111a155
Romera <romeramatias0@gmail.com>
2020-08-28 13:00:35 -0300

    Add Composite Pattern

---
+++ 2-1-Prog2-Clase-03/src/edu/ort/prog2/clase03/Main.java
@@ -0,0 +1,15 @@
+/** @author romeramatias 
+ */
+package edu.ort.prog2.clase03;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
